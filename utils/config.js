f13ec394535672f17a4d098fc2c3e11c24948bc9
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-05-29 17:36:39 +0500

    Изменил порт

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 const {
-  PORT = 3000,
+  PORT = 3001,
   DB = 'mongodb://127.0.0.1:27017/bitfilmsdb',
   JWT_SECRET = 'a1d731321cb2024fe9dfa90167bc4b40c2eefd1a4975d641f3d1484f71f5868c',
 } = process.env;
