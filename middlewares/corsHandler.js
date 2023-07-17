957417edba85d7b3af718be0d9fcdc17fb5bb995
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-07-17 19:14:40 +0500

    fix: исправил адреса в корс-обработчике

--- middlewares/corsHandler.js
+++ middlewares/corsHandler.js
@@ -1,7 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  'https://argayash-movies-explorer.nomoredomains.rocks/',
-  'http://argayash-movies-explorer.nomoredomains.rocks/',
+  'https://argayash-movies-explorer.nomoredomains.rocks',
+  'http://argayash-movies-explorer.nomoredomains.rocks',
   'https://localhost:3000',
   'http://localhost:3000',
 ];
